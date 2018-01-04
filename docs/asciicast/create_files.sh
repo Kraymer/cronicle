5063054dc27390fa18c02ae5b07c4f1d5199b7be
Fabrice Laporte <kraymer@gmail.com>
2018-01-05 00:48:47 +0100

    add asciicast.png

---
+++ docs/asciicast/create_files.sh
@@ -0,0 +1,68 @@
+ #!/bin/bash
+
+touch foobar_2007-12-01.txt
+touch foobar_2007-12-02.txt
+touch foobar_2007-12-03.txt
+touch foobar_2007-12-04.txt
+touch foobar_2007-12-05.txt
+touch foobar_2007-12-06.txt
+touch foobar_2007-12-07.txt
+touch foobar_2007-12-08.txt
+touch foobar_2007-12-09.txt
+touch foobar_2007-12-10.txt
+touch foobar_2007-12-11.txt
+touch foobar_2007-12-12.txt
+touch foobar_2007-12-13.txt
+touch foobar_2007-12-14.txt
+touch foobar_2007-12-15.txt
+touch foobar_2007-12-16.txt
+touch foobar_2007-12-17.txt
+touch foobar_2007-12-18.txt
+touch foobar_2007-12-19.txt
+touch foobar_2007-12-20.txt
+touch foobar_2007-12-21.txt
+touch foobar_2007-12-22.txt
+touch foobar_2007-12-23.txt
+touch foobar_2007-12-24.txt
+touch foobar_2007-12-25.txt
+touch foobar_2007-12-26.txt
+touch foobar_2007-12-27.txt
+touch foobar_2007-12-28.txt
+touch foobar_2007-12-29.txt
+touch foobar_2007-12-30.txt
+touch foobar_2007-12-31.txt
+
+touch -h -t 200712010100 foobar_2007-12-01.txt
+touch -h -t 200712020100 foobar_2007-12-02.txt
+touch -h -t 200712030100 foobar_2007-12-03.txt
+touch -h -t 200712040100 foobar_2007-12-04.txt
+touch -h -t 200712050100 foobar_2007-12-05.txt
+touch -h -t 200712060100 foobar_2007-12-06.txt
+touch -h -t 200712070100 foobar_2007-12-07.txt
+touch -h -t 200712080100 foobar_2007-12-08.txt
+touch -h -t 200712090100 foobar_2007-12-09.txt
+touch -h -t 200712100100 foobar_2007-12-10.txt
+touch -h -t 200712110100 foobar_2007-12-11.txt
+touch -h -t 200712120100 foobar_2007-12-12.txt
+touch -h -t 200712130100 foobar_2007-12-13.txt
+touch -h -t 200712140100 foobar_2007-12-14.txt
+touch -h -t 200712150100 foobar_2007-12-15.txt
+touch -h -t 200712160100 foobar_2007-12-16.txt
+touch -h -t 200712170100 foobar_2007-12-17.txt
+touch -h -t 200712180100 foobar_2007-12-18.txt
+touch -h -t 200712190100 foobar_2007-12-19.txt
+touch -h -t 200712200100 foobar_2007-12-20.txt
+touch -h -t 200712210100 foobar_2007-12-21.txt
+touch -h -t 200712220100 foobar_2007-12-22.txt
+touch -h -t 200712230100 foobar_2007-12-23.txt
+touch -h -t 200712240100 foobar_2007-12-24.txt
+touch -h -t 200712250100 foobar_2007-12-25.txt
+touch -h -t 200712260100 foobar_2007-12-26.txt
+touch -h -t 200712270100 foobar_2007-12-27.txt
+touch -h -t 200712280100 foobar_2007-12-28.txt
+touch -h -t 200712290100 foobar_2007-12-29.txt
+touch -h -t 200712300100 foobar_2007-12-30.txt
+touch -h -t 200712310100 foobar_2007-12-31.txt
+
+
+
