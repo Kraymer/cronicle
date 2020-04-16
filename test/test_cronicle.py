2b64713944fbc104fa6b830336e8327b1d151e92
Fabrice Laporte <kraymer@gmail.com>
2020-04-16 11:17:43 +0200

    Add test test_number_of_archives

--- test/test_cronicle.py
+++ test/test_cronicle.py
@@ -5,6 +5,8 @@ import os
 import tempfile
 import unittest
 import datetime as dt
+import glob
+import mock
 from dateutil import parser
 
 from cronicle import find_config, cronicle
@@ -50,7 +52,7 @@ class Test(unittest.TestCase):
         for date in itertools.islice(date_generator(), 90):
             for hour in (9, 14):
                 abspath = os.path.join(
-                    self.rootdir.name, "foo_{}_{}h".format(str(date), hour)
+                    self.rootdir.name, "foo_{}_{:02d}h".format(str(date), hour)
                 )
                 with open(abspath, "w"):
                     pass
@@ -58,16 +60,28 @@ class Test(unittest.TestCase):
 
         self.last_file = abspath
 
-    def test_archives_folders(self):
-        """Check that no empty archive folder is created."""
-        config.add({os.path.join(self.rootdir.name, "foo_*"): {"daily": 3}})
-        cronicle([self.last_file])
-        self.assertTrue(os.path.exists(os.path.join(self.rootdir.name, "DAILY")))
-        self.assertFalse(
-            any(
-                [
-                    os.path.exists(os.path.join(self.rootdir.name, x))
-                    for x in (u"MONTHLY", u"WEEKLY", u"YEARLY")
-                ]
-            )
+    @mock.patch("cronicle.file_create_day", side_effect=mock_file_create_day)
+    def test_number_of_archives(self, mock):
+        config.add(
+            {
+                os.path.join(self.rootdir.name, "foo_*"): {
+                    "daily": 3,
+                    "weekly": 4,
+                    "monthly": 4,
+                    "yearly": 4,
+                }
+            }
+        )
+        files = sorted(glob.glob(os.path.join(self.rootdir.name, "foo_*")))
+        cronicle(files)
+        self.assertEqual(
+            set(os.listdir(os.path.join(self.rootdir.name, "DAILY"))),
+            {
+                # Archives done at 9h are kept instead of those at 14h as
+                # symlinking the latter fail because of too short delay since
+                # last archive
+                "foo_2020-02-26_09h",
+                "foo_2020-02-27_09h",
+                "foo_2020-02-28_09h",
+            },
         )
